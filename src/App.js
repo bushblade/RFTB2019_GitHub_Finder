07318046c2ab31b058b6ccaa95ceb0fd6d60484e
Will Adams <bushblade@gmail.com>
2019-11-18 09:14:37 +0000

    class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Hello from React</h1>
-    </div>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello from React</h1>
+      </div>
+    )
+  }
 }
 
 export default App
