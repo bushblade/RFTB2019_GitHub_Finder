d91369b7c25ded56f9c14bdd59350e3df33435b3
Will Adams <bushblade@gmail.com>
2019-11-18 09:21:36 +0000

    named import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div className='App'>
         <h1>Hello from React</h1>
+        <h2>Goodbye</h2>
       </div>
     )
   }
