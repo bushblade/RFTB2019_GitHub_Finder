9faeb230091b1c686677778a641014dc065dd118
Will Adams <bushblade@gmail.com>
2019-11-19 14:18:09 +0000

    navbar basics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
+import Navbar from './components/layout/Navbar'
+
+import './App.css'
 
 class App extends Component {
   render() {
-    const name = 'John Doe'
-    const loading = false
-    const showName = true
     return (
       <div className='App'>
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+        <Navbar />
       </div>
     )
   }
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <nav className='navbar bg-primary'>
+        <h1>Navbar</h1>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
