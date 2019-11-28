c3ab0d9af270ed894d89f81124b73c32b3fcf794
Will Adams <bushblade@gmail.com>
2019-11-28 12:45:43 +0000

    lesson complete

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class App extends Component {
   }
   async componentDidMount() {
     this.setState({ loading: true })
-    const res = await axios.get('https://api.github.com/users')
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
     this.setState({ users: res.data, loading: false })
   }
   render() {
