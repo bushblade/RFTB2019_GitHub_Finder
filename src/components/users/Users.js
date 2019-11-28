2b1e632e427eb6e6114197624111e05721995793
Will Adams <bushblade@gmail.com>
2019-11-28 12:36:49 +0000

    lesson complete

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
 
-class Users extends Component {
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.users.map(user => (
+        {users.map(user => (
           <UserItem user={user} key={user.id} />
         ))}
       </div>
@@ -17,6 +21,11 @@ const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
   gridGap: '1rem'
+}
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 }
 
 export default Users
