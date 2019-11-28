c81d1a0a7a71b571871764cf5c9513ea922d4c39
Will Adams <bushblade@gmail.com>
2019-11-28 13:08:40 +0000

    lesson complete

--- src/App.js
+++ src/App.js
@@ -11,19 +11,22 @@ class App extends Component {
     users: [],
     loading: false
   }
-  async componentDidMount() {
+
+  // search github users
+  searchUsers = async text => {
     this.setState({ loading: true })
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     )
-    this.setState({ users: res.data, loading: false })
+    this.setState({ users: res.data.items, loading: false })
   }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class Search extends Component {
   state = {
     text: ''
   }
 
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
+  }
+
   onSubmit = e => {
     e.preventDefault()
-    console.log(this.state.text)
+    this.props.searchUsers(this.state.text)
+    this.setState({ text: '' })
   }
 
   onChange = e => this.setState({ [e.target.name]: e.target.value })
@@ -24,7 +30,7 @@ export class Search extends Component {
           />
           <input
             type='submit'
-            value={this.state.text}
+            value='Search'
             className='btn btn-dark btn-block'
           />
         </form>
