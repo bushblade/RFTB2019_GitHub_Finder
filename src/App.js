7833078d3ca4106745501d8bf7055f4cff963219
Will Adams <bushblade@gmail.com>
2019-11-30 15:38:48 +0000

    lesson complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
-import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
 import About from './components/pages/About'
 import User from './components/users/User'
 import GithubState from './context/github/GitHubState'
 import AlertState from './context/alert/AlertState'
+import Home from './components/pages/Home'
+import NotFound from './components/pages/NotFound'
 import './App.css'
 
 const App = () => {
@@ -21,18 +21,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path={`/user/:login`} component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
