5226b90b7da98928753d10cc9ae2d67c7d5e02c8
Will Adams <bushblade@gmail.com>
2019-12-16 13:30:34 +0000

    .env variables for netlify

--- src/context/github/GitHubState.js
+++ src/context/github/GitHubState.js
@@ -13,11 +13,11 @@ import {
 let githubClientId, githubClientSecret
 
 if (process.env.NODE_ENV !== 'production') {
-  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID
-  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET
-} else {
   githubClientId = process.env.GITHUB_CLIENT_ID
   githubClientSecret = process.env.GITHUB_CLIENT_SECRET
+} else {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET
 }
 
 const GithubState = props => {
