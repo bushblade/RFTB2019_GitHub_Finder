c7bbd11f6b0b47fe9bfc9ee29130ea037d9a20a2
Will Adams <bushblade@gmail.com>
2019-12-17 17:07:16 +0000

    remove unused types

--- src/context/types.js
+++ src/context/types.js
@@ -1,8 +1,6 @@
 export const SEARCH_USERS = 'SEARCH_USERS'
-export const GET_USER = 'GET_USER'
 export const GET_USER_AND_REPOS = 'GET_USER_AND_REPOS'
 export const CLEAR_USERS = 'CLEAR_USERS'
-export const GET_REPOS = 'GET_REPOS'
 export const SET_LOADING = 'SET_LOADING'
 export const SET_ALERT = 'SET_ALERT'
 export const REMOVE_ALERT = 'REMOVE_ALERT'
