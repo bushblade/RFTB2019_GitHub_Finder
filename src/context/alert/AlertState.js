4192cfb566c21d2540540a137927080a59b6c91a
Will Adams <bushblade@gmail.com>
2019-12-18 21:28:54 +0000

    cleaned up alertcontext

--- src/context/alert/AlertState.js
+++ src/context/alert/AlertState.js
@@ -1,20 +1,16 @@
-import React, { useReducer } from 'react'
+import React, { useState } from 'react'
 import AlertContext from './alertContext'
-import alertReducer from './alertReducer'
-import { SET_ALERT, REMOVE_ALERT } from '../types'
 
 const AlertState = props => {
-  const initialState = null
-
-  const [state, dispatch] = useReducer(alertReducer, initialState)
+  const [alert, setAlertState] = useState(null)
 
   const setAlert = (msg, type) => {
-    dispatch({ type: SET_ALERT, payload: { msg, type } })
-    setTimeout(() => dispatch({ type: REMOVE_ALERT }), 5000)
+    setAlertState({ msg, type })
+    setTimeout(() => setAlertState(null), 5000)
   }
 
   return (
-    <AlertContext.Provider value={{ alert: state, setAlert }}>
+    <AlertContext.Provider value={{ alert, setAlert }}>
       {props.children}
     </AlertContext.Provider>
   )
