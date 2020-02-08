3374dea0721d5525d34cc3dd2094b4b3427f56eb
Will Adams <bushblade@gmail.com>
2020-02-08 18:09:16 +0000

    changes to github api

--- src/context/github/actions.js
+++ src/context/github/actions.js
@@ -1,20 +1,20 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const github = axios.create({
   baseURL: 'https://api.github.com',
   timeout: 1000,
-  headers: {Authorization: process.env.REACT_APP_GITHUB_TOKEN},
-});
+  headers: { Authorization: process.env.REACT_APP_GITHUB_TOKEN }
+})
 
 export const searchUsers = async text => {
-  const res = await github.get(`/search/users?q=${text}&`);
-  return res.data.items;
-};
+  const res = await github.get(`/search/users?q=${text}`)
+  return res.data.items
+}
 
 export const getUserAndRepos = async username => {
   const repos = await github.get(
-    `/users/${username}/repos?per_page=5&sort=created:asc?`,
-  );
-  const user = await github.get(`/users/${username}?`);
-  return {user: user.data, repos: repos.data};
-};
+    `/users/${username}/repos?per_page=5&sort=created:asc?`
+  )
+  const user = await github.get(`/users/${username}?`)
+  return { user: user.data, repos: repos.data }
+}
