692aec150f0f2c001e6620c0837a81efb1e498e6
Will Adams <bushblade@gmail.com>
2020-02-08 17:43:00 +0000

    changes to github api query parameters

--- src/context/github/actions.js
+++ src/context/github/actions.js
@@ -1,19 +1,20 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const gitGet = query =>
-  axios.get(
-    `https://api.github.com${query}client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-  )
+const github = axios.create({
+  baseURL: 'https://api.github.com',
+  timeout: 1000,
+  headers: {Authorization: process.env.REACT_APP_GITHUB_TOKEN},
+});
 
 export const searchUsers = async text => {
-  const res = await gitGet(`/search/users?q=${text}&`)
-  return res.data.items
-}
+  const res = await github.get(`/search/users?q=${text}&`);
+  return res.data.items;
+};
 
 export const getUserAndRepos = async username => {
-  const repos = await gitGet(
-    `/users/${username}/repos?per_page=5&sort=created:asc?`
-  )
-  const user = await gitGet(`/users/${username}?`)
-  return { user: user.data, repos: repos.data }
-}
+  const repos = await github.get(
+    `/users/${username}/repos?per_page=5&sort=created:asc?`,
+  );
+  const user = await github.get(`/users/${username}?`);
+  return {user: user.data, repos: repos.data};
+};
