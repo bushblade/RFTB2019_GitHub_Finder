002ba48c73de4918655f87039eb3a983da72f0e9
Will Adams <bushblade@gmail.com>
2020-12-27 18:27:38 +0000

    concurrently fetch

--- src/context/github/actions.js
+++ src/context/github/actions.js
@@ -6,15 +6,15 @@ const github = axios.create({
   headers: { Authorization: process.env.REACT_APP_GITHUB_TOKEN }
 })
 
-export const searchUsers = async text => {
+export const searchUsers = async (text) => {
   const res = await github.get(`/search/users?q=${text}`)
   return res.data.items
 }
 
-export const getUserAndRepos = async username => {
-  const repos = await github.get(
-    `/users/${username}/repos?per_page=5&sort=created:asc?`
-  )
-  const user = await github.get(`/users/${username}?`)
+export const getUserAndRepos = async (username) => {
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${username}?`),
+    github.get(`/users/${username}/repos?per_page=5&sort=created:asc?`)
+  ])
   return { user: user.data, repos: repos.data }
 }
